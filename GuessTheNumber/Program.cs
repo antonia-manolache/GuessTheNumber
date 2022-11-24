35e7880eb1a29dc5c00f693a1d16507cb2b642b9
antonia-manolache <anto.manolache@gmail.com>
2022-11-25 00:03:26 +0200

    I added functions to my program in order to have a cleaner code.

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Globalization;
+
 var lowerLimit = new int();
 var upperLimit = new int();
 
@@ -8,31 +10,136 @@ var guessedNumber = new int();
 
 var random = new Random();
 
-Console.WriteLine("Insert the lower limit of the range: ");
-lowerLimit = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Insert the upper limit of the range: ");
-upperLimit = Convert.ToInt32(Console.ReadLine());
-
-randomNumber = random.Next(lowerLimit, upperLimit);
-
-Console.WriteLine("I am thinking of a number between " + lowerLimit + " and " + upperLimit + ". Try and guess it");
-
-guessedNumber = Convert.ToInt32(Console.ReadLine());
-
-while (guessedNumber != randomNumber)
+bool CheckBoundaries(int lowerLimit, int upperLimit)
 {
-    if (guessedNumber < randomNumber)
+    return lowerLimit <= upperLimit;
+}
+
+void ValidateBoundaries()
+{
+    bool isUpperLimitOk = false;
+    while(!isUpperLimitOk)
     {
-        Console.WriteLine("Number " + guessedNumber + " is too low. Please try again!");
-        guessedNumber = Convert.ToInt32(Console.ReadLine());
-    }
-    else
-    {
-        Console.WriteLine("Number " + guessedNumber + " is too high. Please try again!");
-        guessedNumber = Convert.ToInt32(Console.ReadLine());
+        if(!CheckBoundaries(lowerLimit, upperLimit))
+        {
+            Console.WriteLine("The upper limit is less than the lower limit. Insert lower limit again: ");
+            ReadUpperLimit();
+        }
+        else
+        {
+            isUpperLimitOk = true;
+        }
     }
 }
 
-Console.WriteLine("Congratulation! " + guessedNumber + " is the number I was thinking about.");
+void ReadLowerLimit()
+{
+    bool isLimitValid = false;
+    string value;
 
+   
+   
+    while(!isLimitValid)
+    {
+        Console.WriteLine("Insert the lower limit of the range: ");
+        value = Console.ReadLine();
+        if(int.TryParse(value, out lowerLimit))
+        {
+            isLimitValid = true;
+
+        }
+        else
+        {
+            Console.WriteLine("Invlid limit!");
+        }
+
+    }
+
+}
+
+void ReadUpperLimit()
+{
+    bool isLimitValid = false;
+    string value;
+
+
+
+    while (!isLimitValid)
+    {
+        Console.WriteLine("Insert the upper limit of the range: ");
+        value = Console.ReadLine();
+        if (int.TryParse(value, out upperLimit))
+        {
+            isLimitValid = true;
+
+        }
+        else
+        {
+            Console.WriteLine("Invlid limit!");
+        }
+
+    }
+
+}
+
+void ReadGuessedNumber()
+{
+    bool isNumberValid = false;
+    string value;
+
+
+
+    while (!isNumberValid)
+    {
+        value = Console.ReadLine();
+        if (int.TryParse(value, out guessedNumber))
+        {
+            isNumberValid = true;
+
+        }
+        else
+        {
+            Console.WriteLine("Invlid guess! Please insert an int value: ");
+        }
+
+    }
+
+
+}
+
+void GenerateRandomNumber()
+{
+    Console.WriteLine("I am thinking of a number between " + lowerLimit + " and " + upperLimit + ". Try and guess it");
+    randomNumber = random.Next(lowerLimit, upperLimit);
+}
+
+void KeepGuessing()
+{
+    ReadGuessedNumber();
+
+    while (guessedNumber != randomNumber)
+    {
+        if (guessedNumber < randomNumber)
+        {
+            Console.WriteLine("Number " + guessedNumber + " is too low. Please try again!");
+            ReadGuessedNumber();
+        }
+        else
+        {
+            Console.WriteLine("Number " + guessedNumber + " is too high. Please try again!");
+            ReadGuessedNumber();
+        }
+    }
+
+    Console.WriteLine("Congratulation! " + guessedNumber + " is the number I was thinking about.");
+}
+
+
+ReadLowerLimit();
+ReadUpperLimit();
+
+ValidateBoundaries();
+
+GenerateRandomNumber();
+
+KeepGuessing();
